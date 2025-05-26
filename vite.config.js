407a791d83b825d48f5bc5b5eb0b5304ce765965
Abdulah Murzaev <amrzv.dev@gmail.com>
2025-05-26 18:48:03 +0200

    Package JSON edit 3

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/murzaev-dev",
   plugins: [
     tailwindcss(),
     react()
   ],
-  base: "/murzaev-dev",
 })
